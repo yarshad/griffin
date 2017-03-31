da11d327b998878d50a15933b2c74fcc04d4943f
yarshad <yarshad22@gmail.com>
2017-03-30 21:04:09 -0400

    added http

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response} from '@angular/http'
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    optionChain;
+    ticker = null;
+    constructor(private _http: Http) {}
 
-  ngOnInit() {
-  }
+    getOptionChain(){
+        var url = 'http://griffin-api.herokuapp.com/quotes/AAPL'
+        return this._http.get(url).map((response: Response) => response.json());
+                
+    }
+
+    ngOnInit() {
+      this.getOptionChain().subscribe(optionData =>{
+              console.log(optionData);
+              this.ticker = optionData.symbol;
+              this.optionChain = optionData
+      });
+    }
 
  title = 'Welcome to Griffin!';
 }
