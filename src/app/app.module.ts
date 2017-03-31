67d3c2b0e2411b100376d289c3fcfedc85ae14a8
yarshad <yarshad22@gmail.com>
2017-03-30 20:49:25 -0400

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent    
+    NavComponent,
+    HomeComponent    
   ],
   imports: [
     BrowserModule,
